6f639e1d79790087d8ed1eeb647dd5f8e35c5f4a
alexein777 <38079932+alexein777@users.noreply.github.com>
2022-03-17 14:55:49 +0100

    Split clients into 3 csvs

--- main.py
+++ main.py
@@ -1,10 +1,30 @@
 import requests
 import pandas as pd
+import sys
+import os
 
 
 def main():
-    root_url = 'https://raw.githubusercontent.com/magaton/ce/master/test/volumes-and-contributions/'
+    if len(sys.argv) < 2:
+        raise ValueError('args')
 
+    args = sys.argv[1:]
+    root_url = args[0]
+    output_path = args[1] if len(args) > 1 else os.path.join('.', 'tmp')
+    csv_endpoint = '/clients.csv'
+    clients_endpoint = root_url + csv_endpoint
+    print(clients_endpoint)
+
+    df = pd.read_csv(clients_endpoint)
+    features = df.columns
+    print(features)
+    clients = df.drop(columns=['sponsor', 'leader', 'startDate', 'endDate'])
+    sponsorship = df.loc[:, ['id', 'login', 'sponsor', 'startDate', 'endDate']]
+    leadership = df.loc[:, ['id', 'login', 'startDate', 'endDate']]
+
+    clients.to_csv(os.path.join(output_path, 'clients.csv'))
+    sponsorship.to_csv(os.path.join(output_path, 'sponsorship.csv'))
+    leadership.to_csv(os.path.join(output_path, 'leadership.csv'))
 
 
 if __name__ == '__main__':
