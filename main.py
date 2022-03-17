aeaa2363ee568038da8944e4ce325ae3edcba19e
alexein777 <38079932+alexein777@users.noreply.github.com>
2022-03-17 14:32:50 +0100

    initial

---
+++ main.py
@@ -0,0 +1,11 @@
+import requests
+import pandas as pd
+
+
+def main():
+    root_url = 'https://raw.githubusercontent.com/magaton/ce/master/test/volumes-and-contributions/'
+
+
+
+if __name__ == '__main__':
+    main()
