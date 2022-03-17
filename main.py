83d5326841df42a0057259343ca0ff7f074ba915
alexein777 <38079932+alexein777@users.noreply.github.com>
2022-03-17 15:10:10 +0100

    convert ranking from float to int

--- main.py
+++ main.py
@@ -19,6 +19,9 @@ def main():
     features = df.columns
     print(features)
     clients = df.drop(columns=['sponsor', 'leader', 'startDate', 'endDate'])
+    clients = clients.convert_dtypes()
+    print(clients.dtypes)
+
     sponsorship = df.loc[:, ['id', 'login', 'sponsor', 'startDate', 'endDate']]
     leadership = df.loc[:, ['id', 'login', 'startDate', 'endDate']]
 
